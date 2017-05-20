52cf7c4f4a636ef869c4d5070a8e87d214918ab8
Lee Verberne <verb@google.com>
2017-05-20 17:38:05 +0000

    clean up into MVP for kubectl debug

--- pkg/kubectl/cmd/debug.go
+++ pkg/kubectl/cmd/debug.go
@@ -37,21 +37,14 @@ import (
 )
 
 var (
-	// TODO(verb)
 	debug_example = templates.Examples(`
-		# Get output from running 'date' from pod 123456-7890, using the first container by default
-		kubectl debug 123456-7890 date
-
-		# Get output from running 'date' in ruby-container from pod 123456-7890
-		kubectl debug 123456-7890 -c ruby-container date
-
-		# Switch to raw terminal mode, sends stdin to 'bash' in ruby-container from pod 123456-7890
-		# and sends stdout/stderr from 'bash' back to the client
-		kubectl debug 123456-7890 -c ruby-container -i -t -- bash -il`)
+		# Switch to raw terminal mode, sends stdin to 'bash' in a new container in pod 123456-7890
+		# named 'debug' from image 'debian and sends stdout/stderr from 'bash' back to the client
+		kubectl debug 123456-7890 -c debug -m debian -i -t -- bash -il`)
 )
 
 const (
-	debugUsageStr             = "expected 'debug POD_NAME COMMAND [ARG1] [ARG2] ... [ARGN]'.\nPOD_NAME and COMMAND are required arguments for the debug command"
+	debugUsageStr             = "expected 'debug POD_NAME [--] [COMMAND [ARG1 ... ARGN]]'.\nPOD_NAME is a required argument for the debug command.\nCOMMAND may be required if the container does not provide an ENTRYPOINT"
 	debugDefaultContainerName = "debug"
 	debugDefaultImageName     = "debian"
 )
@@ -72,18 +65,16 @@ func NewCmdDebug(f cmdutil.Factory, cmdIn io.Reader, cmdOut, cmdErr io.Writer) *
 	}
 	cmd := &cobra.Command{
 		Use:     "debug POD [-c CONTAINER] -- COMMAND [args...]",
-		Short:   i18n.T("Debug a command in a container"),
-		Long:    "Debug a command in a container.",
+		Short:   i18n.T("Run a debug container in a pod"),
+		Long:    "Run a new container inside an existing pod.",
 		Example: debug_example,
 		Run: func(cmd *cobra.Command, args []string) {
 			argsLenAtDash := cmd.ArgsLenAtDash()
 			cmdutil.CheckErr(options.Complete(f, cmd, args, argsLenAtDash))
 			cmdutil.CheckErr(options.Validate())
-			cmdutil.CheckErr(options.Run())
+			cmdutil.CheckErr(options.RunDebug())
 		},
 	}
-	cmd.Flags().StringVarP(&options.PodName, "pod", "p", "", "Pod name")
-	// TODO support UID
 	cmd.Flags().StringVarP(&options.ContainerName, "container", "c", "", fmt.Sprintf("Container name. [default=%s]", debugDefaultContainerName))
 	cmd.Flags().StringVarP(&options.ImageName, "image", "m", "", fmt.Sprintf("Image name. [default=%s]", debugDefaultImageName))
 	cmd.Flags().BoolVarP(&options.Stdin, "stdin", "i", false, "Pass stdin to the container")
@@ -121,8 +112,7 @@ type DebugOptions struct {
 
 	Command []string
 
-	FullCmdName       string
-	SuggestedCmdUsage string
+	FullCmdName string
 
 	Debugger  RemoteDebugger
 	PodClient coreclient.PodsGetter
@@ -131,30 +121,24 @@ type DebugOptions struct {
 
 // Complete verifies command line arguments and loads data from the command environment
 func (p *DebugOptions) Complete(f cmdutil.Factory, cmd *cobra.Command, argsIn []string, argsLenAtDash int) error {
-	// Let kubectl debug follow rules for `--`, see #13004 issue
-	if len(p.PodName) == 0 && (len(argsIn) == 0 || argsLenAtDash == 0) {
+	if len(argsIn) == 0 || argsLenAtDash == 0 {
 		return cmdutil.UsageError(cmd, debugUsageStr)
 	}
-	if len(p.PodName) != 0 {
-		printDeprecationWarning("debug POD_NAME", "-p POD_NAME")
-		if len(argsIn) < 1 {
-			return cmdutil.UsageError(cmd, debugUsageStr)
-		}
-		p.Command = argsIn
-	} else {
-		p.PodName = argsIn[0]
-		p.Command = argsIn[1:]
-		if len(p.Command) < 1 {
-			return cmdutil.UsageError(cmd, debugUsageStr)
-		}
+	p.PodName = argsIn[0]
+	p.Command = argsIn[1:]
+
+	if len(p.ContainerName) == 0 {
+		fmt.Fprintf(p.Err, "Defaulting container name to %q.\n", debugDefaultContainerName)
+		p.ContainerName = debugDefaultContainerName
+	}
+	if len(p.ImageName) == 0 {
+		fmt.Fprintf(p.Err, "Defaulting image name to %q.\n", debugDefaultImageName)
+		p.ImageName = debugDefaultImageName
 	}
 
 	cmdParent := cmd.Parent()
 	if cmdParent != nil {
 		p.FullCmdName = cmdParent.CommandPath()
-	}
-	if len(p.FullCmdName) > 0 && cmdutil.IsSiblingCommandExists(cmd, "describe") {
-		p.SuggestedCmdUsage = fmt.Sprintf("Use '%s describe pod/%s' to see all of the containers in this pod.", p.FullCmdName, p.PodName)
 	}
 
 	namespace, _, err := f.DefaultNamespace()
@@ -183,9 +167,6 @@ func (p *DebugOptions) Validate() error {
 	if len(p.PodName) == 0 {
 		return fmt.Errorf("pod name must be specified")
 	}
-	if len(p.Command) == 0 {
-		return fmt.Errorf("you must specify at least one command for the container")
-	}
 	if p.Out == nil || p.Err == nil {
 		return fmt.Errorf("both output and error output must be provided")
 	}
@@ -195,37 +176,15 @@ func (p *DebugOptions) Validate() error {
 	return nil
 }
 
-// Run debugs a validated remote debugging against a pod.
-func (p *DebugOptions) Run() error {
+// RunDebug debugs a validated remote debugging against a pod.
+func (p *DebugOptions) RunDebug() error {
 	pod, err := p.PodClient.Pods(p.Namespace).Get(p.PodName, metav1.GetOptions{})
 	if err != nil {
 		return err
 	}
 
-	/*
-		if pod.Status.Phase == api.PodSucceeded || pod.Status.Phase == api.PodFailed {
-			return fmt.Errorf("cannot debug into a container in a completed pod; current phase is %s", pod.Status.Phase)
-		}
-	*/
-
-	containerName := p.ContainerName
-	if len(containerName) == 0 {
-		usageString := fmt.Sprintf("Defaulting container name to %s.", debugDefaultContainerName)
-		if len(p.SuggestedCmdUsage) > 0 {
-			usageString = fmt.Sprintf("%s\n%s", usageString, p.SuggestedCmdUsage)
-		}
-		fmt.Fprintf(p.Err, "%s\n", usageString)
-		containerName = debugDefaultContainerName
-	}
-
-	imageName := p.ImageName
-	if len(imageName) == 0 {
-		usageString := fmt.Sprintf("Defaulting image name to %s.", debugDefaultImageName)
-		if len(p.SuggestedCmdUsage) > 0 {
-			usageString = fmt.Sprintf("%s\n%s", usageString, p.SuggestedCmdUsage)
-		}
-		fmt.Fprintf(p.Err, "%s\n", usageString)
-		imageName = debugDefaultImageName
+	if pod.Status.Phase == api.PodSucceeded || pod.Status.Phase == api.PodFailed {
+		return fmt.Errorf("cannot debug into a container in a completed pod; current phase is %s", pod.Status.Phase)
 	}
 
 	// ensure we can recover the terminal while attached
@@ -247,15 +206,14 @@ func (p *DebugOptions) Run() error {
 			return err
 		}
 
-		// TODO: consider abstracting into a client invocation or client helper
 		req := restClient.Post().
 			Resource("pods").
 			Name(pod.Name).
 			Namespace(pod.Namespace).
 			SubResource("debug")
 		req.VersionedParams(&api.PodDebugOptions{
-			Container: containerName,
-			Image:     imageName,
+			Container: p.ContainerName,
+			Image:     p.ImageName,
 			Command:   p.Command,
 			Stdin:     p.Stdin,
 			Stdout:    p.Out != nil,
